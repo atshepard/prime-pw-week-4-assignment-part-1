7847cb80ee55fe1f744500dd4d5025895cbc2e86
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-02-09 14:26:51 -0600

    building out process for step 6

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -42,9 +42,9 @@ console.log(multiplyThree(10, 100, 15));
 // 5. Function that will return true if a number is positive,
 //    or greater than zero, and false otherwise
 function isPositive( number ) {
-  if ( number > 0 ){
+  if ( number > 0 ) {
     return true;
-  } else if ( number < 0 ) {
+  } else if (number <= 0) {
     return false;
   }
 }
@@ -57,17 +57,30 @@ console.log( 'isPositive - should say false', isPositive(-3) );
 
 // 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
-function getLast( array ) {
 
+/* function getLast( array ) {
+  return array[array.length-1];
+} */
+
+// let array = []
+function getLast (array) {
+  return array[array.length - 1];
 }
+
+console.log(getLast(array[0, 1, 2, 3]));
+console.log(getLast(array));
+console.log(getLast(array['Leo', 'Nyx', 'Andie']));
 
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
 function find( value, array ){
-
+for (let value of array) {
+  return true;
+}
 }
 
+console.log()
 // ----------------------
 // Stretch Goals
 // ----------------------

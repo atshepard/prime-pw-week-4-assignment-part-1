7a6a8b659b417d8cd178bfb95057319442400a66
Ashlyn Shepard <ashlyntshepard@gmail.com>
2022-02-10 21:39:02 -0600

    added solution for stretch question 8

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -123,10 +123,14 @@ console.log('Stretch Goals:')
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+if (letter == string.charAt(0)) {
+  return true; //should be able to use return here, because we CAN stop the function. No array to iterate through.
+} else {
+  return false; //if there were objects or an array to iterate through, or a loop inside the function, I would have to use something other than 'return'
 }
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+}
+console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') ); //logs true
+console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') ); //logs false
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
